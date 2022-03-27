42687cbc3aa617e9a7d13bd264a92bcfe5487ce0
Gwendoline8 <gwendoline.marek@edu.devinci.fr>
2022-03-27 12:40:56 +0000

    Created a function to sort orders

--- book.py
+++ book.py
@@ -19,11 +19,24 @@ class Book:
     
     def __str__(self):
         return(f"\nBook on {self.name}\n" + '\n'.join(list(map(str, self.orders))) + "\n------------------------")
-    
+   
+    def sort_orders(self):
+        sell_orders = []
+        buy_orders = []
+        for order in self.orders:
+            if(order.buy):
+                buy_orders.append(order)
+            else:
+                sell_orders.append(order)
+        sell_orders.sort(key = lambda x: x.price)
+        buy_orders.sort(key = lambda x: -x.price)
+        self.orders = sell_orders + buy_orders
+
     def insert_buy(self, qty, price):
         buy_order = Order(order_id = self.next_order_id, qty = qty, price = price)
         self.orders.append(buy_order)
         self.next_order_id += 1
+        self.sort_orders()
         print(f'--- Insert BUY {buy_order.qty}@{buy_order.price} id={buy_order.order_id} on {self.name}')
         print(self)
 
@@ -31,5 +44,6 @@ class Book:
         sell_order = Order(order_id = self.next_order_id, qty = qty, price = price, buy = False)
         self.orders.append(sell_order)
         self.next_order_id += 1
+        self.sort_orders()
         print(f'--- Insert SELL {sell_order.qty}@{sell_order.price} id={sell_order.order_id} on {self.name}')
         print(self)
