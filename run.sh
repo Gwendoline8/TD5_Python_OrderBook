735af1855a0f9d438d3edb5fb478befd163d18dd
MaureyCesar <cesarmaurey@hotmail.com>
2022-03-27 12:58:41 +0000

    Corrected run.sh file

--- run.sh
+++ run.sh
@@ -2,15 +2,23 @@
 
 if [[ -d ".env" ]]
 then
+        echo "Activating virtual environment..."
+        source .env/bin/activate
+        echo "Virtual environment activated"
+else
+        echo "Creating virtual environment..."
+        virtualenv .env
+        echo "Virtual environment created"
 	echo "Activating virtual environment..."
 	source .env/bin/activate
-	ls
 	echo "Virtual environment activated"
-else
-	echo "Creating virtual environment ..."
-	virtualenv .env
-	pip freeze > requirements.txt
-	echo "Virtual environment created"
-	echo "Run main.py"
-	python main.py
+        echo "Loading dependencies..."
+        FILE=requirements.txt
+        if test -f "$FILE";then
+                pip install -r FILE
+        else
+                echo "There is no $FILE file, failed to load dependencies..."
+        fi
 fi
+echo "Running firstFile.py..."
+python firstFile.py
