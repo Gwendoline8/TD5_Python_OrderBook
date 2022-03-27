ee12b44539fbc02e93ffa6f1208ded40b9245e38
MaureyCesar <cesar.maurey@edu.devinci.fr>
2022-03-27 12:22:33 +0000

    Modified print function for Book class and resolved some bugs

--- book.py
+++ book.py
@@ -15,27 +15,21 @@ class Book:
     def __init__(self, name):
         self.name = name
         self.orders = []
-        self.order_count = 1
-
+        self.next_order_id = 1
+    
+    def __str__(self):
+        return(f"\nBook on {self.name}\n" + '\n'.join(list(map(str, self.orders))) + "\n------------------------")
+    
     def insert_buy(self, qty, price):
-        buy_order = Order(order_id = self.order_count, side = "BUY", qty = qty, price = price)
+        buy_order = Order(order_id = self.next_order_id, qty = qty, price = price)
         self.orders.append(buy_order)
-        self.order_count += 1
-        print(f'--- Insert {buy_order.side} {buy_order.qty}@{buy_order.price} id={buy_order.order_id} on {self.name}\nBook on {self.name}')
-        #self.print_insertion(buy_order)
-        self.print_book()
+        self.next_order_id += 1
+        print(f'--- Insert BUY {buy_order.qty}@{buy_order.price} id={buy_order.order_id} on {self.name}')
+        print(self)
 
     def insert_sell(self, qty, price):
-        sell_order = Order(order_id = self.order_count, side = "SELL", qty = qty, price = price)
+        sell_order = Order(order_id = self.next_order_id, qty = qty, price = price, buy = False)
         self.orders.append(sell_order)
-        self.order_count += 1
-        print(f'--- Insert {sell_order.side} {sell_order.qty}@{sell_order.price} id={sell_order.order_id} on {self.name}\nBook on {self.name}')
-        #self.print_insertion(sell_order)
-        self.print_book()
-
-    def print_book(self):
-        self.orders.sort(key = lambda x: x.price, reverse = True)
-        for order in self.orders:
-            if (order.qty > 0):
-                print(f'\t{order.side} {order.qty}@{order.price} id={order.order_id}')
-        print("-----------------------")
+        self.next_order_id += 1
+        print(f'--- Insert SELL {sell_order.qty}@{sell_order.price} id={sell_order.order_id} on {self.name}')
+        print(self)
